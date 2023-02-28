8d1e7f4d2f410602130c823e456cb51f963e031f
emilychima <meetchimaemily@gmail.com>
2023-02-28 09:20:07 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,7 +6,7 @@
  * @src: char type string
  * Description: Copy the string pointed to by pointer 'src' to
  * the buffer pointed to by 'dest'
- * Return; Pointer to 'dest
+ * Return: Pointer to 'dest
  */
 
 char *_strcpy(char *dest, char *src)
